a440bb8e10997d9db9941d7626607e672c35c695
joaoh <joao_camargo10@hotmail.com>
2020-02-08 11:30:34 -0300

    Login login login

--- src/modules/auth/pages/Login.vue
+++ src/modules/auth/pages/Login.vue
@@ -1,14 +1,58 @@
 <template>
-  <div>
-    <h1>Login</h1>
-  </div>
+  <form @submit.prevent="submit()">
+    <div class="login-page">
+      <div class="card">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="E-mail" v-model="form.email" />
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              class="form-control"
+              placeholder="Password"
+              v-model="form.password"
+            />
+          </div>
+
+          <button class="btn btn-primary w-100">Sign In</button>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
+import { mapActions } from 'vuex'
 
+
+export default {
+  data: () => ({
+    form: {
+      email: '',
+      password: '',
+    }
+  }),
+
+  methods: {
+    ...mapActions('auth', ['ActionDoLogin']), // params: namespaced e nome da actions
+    submit() {
+      this.ActionDoLogin(this.form).then(res => console.log(res.data))
+    }
+  }
 }
 </script>
 
 <style>
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card {
+  width: 40%;
+}
 </style>
