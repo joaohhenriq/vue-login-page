c6636d1274e29014a10863f2d16fbd97c92104d7
joaoh <joao_camargo10@hotmail.com>
2020-02-09 10:36:59 -0300

    loading serie info

--- src/pages/show-serie/ShowSerie.vue
+++ src/pages/show-serie/ShowSerie.vue
@@ -1,21 +1,46 @@
 <template>
-  <div>{{serie}}</div>
+  <div class="page-serie">
+    <div style="font-size: 1.3rem" v-if="loading">Please, wait...</div>
+    <template v-else>
+      <h1>{{serie.title}}</h1>
+      <img :src="serie.image" height="400px" alt="Poster" loading="lazy" />
+    </template>
+  </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
 export default {
-  created() {
-    this.ActionFindSerie(this.$route.params.id)
+  data: () => ({ loading: true }),
+  mounted() {
+    this.getData()
   },
   computed: {
     ...mapState('showSerie', ['serie'])
   },
   methods: {
-    ...mapActions('showSerie', ['ActionFindSerie'])
+    ...mapActions('showSerie', ['ActionFindSerie']),
+    async getData() {
+      try {
+        await this.ActionFindSerie(this.$route.params.id)
+      } catch (error) {
+        alert('Failed!!')
+      } finally {
+        this.loading = false
+      }
+    }
   }
 }
 </script>
 
 <style>
+.page-serie {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: calc(
+    100vh - 56px
+  ); /* 56px da navbar, para ver altura basta inspecionar o elemento */
+}
 </style>
